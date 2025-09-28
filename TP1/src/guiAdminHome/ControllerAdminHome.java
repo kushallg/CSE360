95854aa3ba8eb488270b5a400a71cf61ab96390a
Amaan Sayed <amaan.sayed729@gmail.com>
2025-09-27 20:05:10 -0700

    feat: Implement multi-role invites and redirect to login

--- TP1/src/guiAdminHome/ControllerAdminHome.java
+++ TP1/src/guiAdminHome/ControllerAdminHome.java
@@ -3,57 +3,13 @@ package guiAdminHome;
 import database.Database;
 import emailAddressValidator.EmailAddressRecognizer;
 import javafx.scene.control.Label;
-
-
-
-/*******
- * <p> Title: GUIAdminHomePage Class. </p>
- * 
- * <p> Description: The Java/FX-based Admin Home Page.  This class provides the controller actions
- * basic on the user's use of the JavaFX GUI widgets defined by the View class.
- * 
- * This page contains a number of buttons that have not yet been implemented.  WHen those buttons
- * are pressed, an alert pops up to tell the user that the function associated with the button has
- * not been implemented. Also, be aware that What has been implemented may not work the way the
- * final product requires and there maybe defects in this code.
- * 
- * The class has been written assuming that the View or the Model are the only class methods that
- * can invoke these methods.  This is why each has been declared at "protected".  Do not change any
- * of these methods to public.</p>
- * 
- * <p> Copyright: Lynn Robert Carter © 2025 </p>
- * 
- * @author Lynn Robert Carter
- * 
- * @version 1.00		2025-08-17 Initial version
- *  
- */
+import java.util.List; // Required for using the List interface
 
 public class ControllerAdminHome {
 	
-	/*-*******************************************************************************************
-
-	User Interface Actions for this page
-	
-	This controller is not a class that gets instantiated.  Rather, it is a collection of protected
-	static methods that can be called by the View (which is a singleton instantiated object) and 
-	the Model is often just a stub, or will be a singleton instantiated object.
-	
-	*/
-
-	// Reference for the in-memory database so this package has access
 	private static Database theDatabase = applicationMain.FoundationsMain.database;
 
-	/**********
-	 * <p> 
-	 * 
-	 * Title: performInvitation () Method. </p>
-	 * 
-	 * <p> Description: Protected method to send an email inviting a potential user to establish
-	 * an account and a specific role. </p>
-	 */
 	protected static void performInvitation () {
-		// Verify that the email address is valid - If not alert the user and return
 		String emailAddress = ViewAdminHome.text_InvitationEmailAddress.getText();
 		
 		String emailErr = EmailAddressRecognizer.checkEmailAddress(emailAddress);
@@ -66,7 +22,7 @@ public class ControllerAdminHome {
 		    
 		    ViewAdminHome.alertEmailError.setTitle("*** ERROR ***");
 		    ViewAdminHome.alertEmailError.setHeaderText("Email Address Validation Error");
-		    ViewAdminHome.alertEmailError.getDialogPane().setContent(label); //Display specific password error
+		    ViewAdminHome.alertEmailError.getDialogPane().setContent(label);
 		    ViewAdminHome.alertEmailError.showAndWait();
 		    return;
 		}
@@ -74,8 +30,6 @@ public class ControllerAdminHome {
 			return;
 		}
 		
-		// Check to ensure that we are not sending a second message with a new invitation code to
-		// the same email address.  
 		if (theDatabase.emailaddressHasBeenUsed(emailAddress)) {
 			ViewAdminHome.alertEmailError.setContentText(
 					"An invitation has already been sent to this email address.");
@@ -83,30 +37,35 @@ public class ControllerAdminHome {
 			return;
 		}
 		
-		// Inform the user that the invitation has been sent and display the invitation code
-		String theSelectedRole = (String) ViewAdminHome.combobox_SelectRole.getValue();
+		// *** CHANGE ***: This block retrieves the list of selected roles from the ListView.
+		// The getSelectedItems() method returns a List of all roles the user has selected.
+		List<String> selectedRoles = ViewAdminHome.listView_Roles.getSelectionModel().getSelectedItems();
+
+        // Ensure at least one role is selected before proceeding.
+        if (selectedRoles.isEmpty()) {
+            ViewAdminHome.alertEmailError.setContentText("Please select at least one role for the invitation.");
+            ViewAdminHome.alertEmailError.showAndWait();
+            return;
+        }
+
+        // *** CHANGE ***: Combines the list of selected roles into a single comma-separated string.
+        // For example, if "Admin" and "Student" are selected, this will create the string "Admin, Student".
+        // This string is what gets stored in the database.
+        String roles = String.join(", ", selectedRoles);
+		
 		String invitationCode = theDatabase.generateInvitationCode(emailAddress,
-				theSelectedRole);
-		String msg = "Code: " + invitationCode + " for role " + theSelectedRole + 
+				roles);
+		String msg = "Code: " + invitationCode + " for role(s) " + roles + 
 				" was sent to: " + emailAddress;
 		System.out.println(msg);
 		ViewAdminHome.alertEmailSent.setContentText(msg);
 		ViewAdminHome.alertEmailSent.showAndWait();
 		
-		// Update the Admin Home pages status
 		ViewAdminHome.text_InvitationEmailAddress.setText("");
 		ViewAdminHome.label_NumberOfInvitations.setText("Number of outstanding invitations: " + 
 				theDatabase.getNumberOfInvitations());
 	}
 	
-	/**********
-	 * <p> 
-	 * 
-	 * Title: manageInvitations () Method. </p>
-	 * 
-	 * <p> Description: Protected method that is currently a stub informing the user that
-	 * this function has not yet been implemented. </p>
-	 */
 	protected static void manageInvitations () {
 		System.out.println("\n*** WARNING ***: Manage Invitations Not Yet Implemented");
 		ViewAdminHome.alertNotImplemented.setTitle("*** WARNING ***");
@@ -115,14 +74,6 @@ public class ControllerAdminHome {
 		ViewAdminHome.alertNotImplemented.showAndWait();
 	}
 	
-	/**********
-	 * <p> 
-	 * 
-	 * Title: setOnetimePassword () Method. </p>
-	 * 
-	 * <p> Description: Protected method that is currently a stub informing the user that
-	 * this function has not yet been implemented. </p>
-	 */
 	protected static void setOnetimePassword () {
 		System.out.println("\n*** WARNING ***: One-Time Password Not Yet Implemented");
 		ViewAdminHome.alertNotImplemented.setTitle("*** WARNING ***");
@@ -131,14 +82,6 @@ public class ControllerAdminHome {
 		ViewAdminHome.alertNotImplemented.showAndWait();
 	}
 	
-	/**********
-	 * <p> 
-	 * 
-	 * Title: deleteUser () Method. </p>
-	 * 
-	 * <p> Description: Protected method that is currently a stub informing the user that
-	 * this function has not yet been implemented. </p>
-	 */
 	protected static void deleteUser() {
 		System.out.println("\n*** WARNING ***: Delete User Not Yet Implemented");
 		ViewAdminHome.alertNotImplemented.setTitle("*** WARNING ***");
@@ -147,14 +90,6 @@ public class ControllerAdminHome {
 		ViewAdminHome.alertNotImplemented.showAndWait();
 	}
 	
-	/**********
-	 * <p> 
-	 * 
-	 * Title: listUsers () Method. </p>
-	 * 
-	 * <p> Description: Protected method that is currently a stub informing the user that
-	 * this function has not yet been implemented. </p>
-	 */
 	protected static void listUsers() {
 		System.out.println("\n*** WARNING ***: List Users Not Yet Implemented");
 		ViewAdminHome.alertNotImplemented.setTitle("*** WARNING ***");
@@ -163,33 +98,11 @@ public class ControllerAdminHome {
 		ViewAdminHome.alertNotImplemented.showAndWait();
 	}
 	
-	/**********
-	 * <p> 
-	 * 
-	 * Title: addRemoveRoles () Method. </p>
-	 * 
-	 * <p> Description: Protected method that allows an admin to add and remove roles for any of
-	 * the users currently in the system.  This is done by invoking the AddRemoveRoles Page. There
-	 * is no need to specify the home page for the return as this can only be initiated by and
-	 * Admin.</p>
-	 */
 	protected static void addRemoveRoles() {
 		guiAddRemoveRoles.ViewAddRemoveRoles.displayAddRemoveRoles(ViewAdminHome.theStage, 
 				ViewAdminHome.theUser);
 	}
 	
-	/**********
-	 * <p> 
-	 * 
-	 * Title: invalidEmailAddress () Method. </p>
-	 * 
-	 * <p> Description: Protected method that is intended to check an email address before it is
-	 * used to reduce errors.  The code currently only checks to see that the email address is not
-	 * empty.  In the future, a syntactic check must be performed and maybe there is a way to check
-	 * if a properly email address is active.</p>
-	 * 
-	 * @param emailAddress	This String holds what is expected to be an email address
-	 */
 	protected static boolean invalidEmailAddress(String emailAddress) {
 		if (emailAddress.length() == 0) {
 			ViewAdminHome.alertEmailError.setContentText(
@@ -200,26 +113,10 @@ public class ControllerAdminHome {
 		return false;
 	}
 	
-	/**********
-	 * <p> 
-	 * 
-	 * Title: performLogout () Method. </p>
-	 * 
-	 * <p> Description: Protected method that logs this user out of the system and returns to the
-	 * login page for future use.</p>
-	 */
 	protected static void performLogout() {
 		guiUserLogin.ViewUserLogin.displayUserLogin(ViewAdminHome.theStage);
 	}
 	
-	/**********
-	 * <p> 
-	 * 
-	 * Title: performQuit () Method. </p>
-	 * 
-	 * <p> Description: Protected method that gracefully terminates the execution of the program.
-	 * </p>
-	 */
 	protected static void performQuit() {
 		System.exit(0);
 	}
