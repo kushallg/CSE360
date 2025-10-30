57ee45b5c5ec48b70eb7e1e5a2b6170e72a4b20c
Amaan Sayed <amaan.sayed729@gmail.com>
2025-10-30 16:33:13 -0700

    Bernice jUnit tests pushed

--- TP1/src/module-info.java
+++ TP1/src/module-info.java
@@ -1,6 +1,7 @@
 module FoundationsF25 {
     requires javafx.controls;
     requires java.sql;
+    requires junit;
 
     opens applicationMain to javafx.graphics, javafx.fxml;
     opens entityClasses to javafx.base;
